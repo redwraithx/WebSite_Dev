b69d447f31d59558c48863efae4be830e28f3fbc
Brian Purdy <brianpurdy01@gmail.com>
2021-09-06 15:46:27 -0400

    Update landingpage.js

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -21,6 +21,34 @@ class Landing extends Component {
 
               <p>[ C# | <img src={process.env.PUBLIC_URL + "/Images/Icons/unityIcon32x32.png"} alt="Unity Logo Image" /> Unity3D | C++ | <img src={process.env.PUBLIC_URL + "/Images/Icons/unrealIcon32x32.png"} alt="Unreal Logo Image" /> Unreal ]</p>
 
+
+              <div style={{ padding: '10px' }}>
+                <div style={{ padding: '20px', textAlign: 'left', color: 'gray' }}>
+
+                  <h5><u>About Me</u></h5>
+
+                    I am a recent graduate of the Toronto Film School where I received a Diploma in Game Programming and Design. 
+                    I am searching for work where being a part of a team means delivering great games and content to people who love to Game!  
+                    I dream of being a part of a company that creates games that inspire the next generation of players and programmers.
+                    As someone committed to self-growth and ongoing learning, I am currently working on programming a new game having turned off the internal game physics. 
+                    All the physics is kinematically programmed and the game is state-based AI.
+
+                </div>
+                <div style={{ padding: '20px', textAlign: 'left', color: 'gray' }}>
+
+                  <h5><u>My Role with our Capstone Project</u></h5>
+                  
+                  I was the Programming Director and assisted the programmers in code or with design direction. 
+                  I was also the sole Networking programmer.
+                  Working as a team in several roles, I helped create multiple game play systems including the machines, 
+                  the item and item-grab programming, the game and audio manager, and the level manager, 
+
+
+                </div>
+              </div>
+
+
+
               <div className="social-links">
 
                 {/* LinkedIn */}
@@ -42,10 +70,12 @@ class Landing extends Component {
             </div>
           </Cell>
         </Grid>
+        <div style={{ paddingTop: '200px' }} />
 
-        <Footer size="2" style={{  position: 'absolute', left: '0', bottom: '0', right: '0', color: '#4e4e4e' }}>
-          <div align="center">BrianPurdy.ca @ 2021, best viewed @ 1920x1080 resolution.</div>
-        </Footer>
+          <Footer size="2" style={{  position: 'absolute', left: '0', bottom: '0', right: '0', color: '#4e4e4e' }}>
+            <div align="center">BrianPurdy.ca @ 2021, best viewed @ 1920x1080 resolution.</div>
+          </Footer>
+        
 
       </div>
 
