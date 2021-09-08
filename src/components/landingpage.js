b4abe1fd9ec936bdca7f047d0195166c5fa5c133
Brian Purdy <brianpurdy01@gmail.com>
2021-09-07 23:04:43 -0400

    minor updates

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -23,23 +23,16 @@ class Landing extends Component {
               <div style={{ padding: '10px', overflow: 'auto', paddingTop: '2px' }} >
                 <div style={{ padding: '20px', textAlign: 'left', color: 'gray' }}>
 
-                  <h5><u>About Me</u></h5>
+                  <h5><u>My journey</u></h5>
 
-                    I am a recent graduate of the Toronto Film School where I received a Diploma in Game Programming and Design. 
-                    I am searching for work where being a part of a team means delivering great games and content to people who love to Game!  
-                    I dream of being a part of a company that creates games that inspire the next generation of players and programmers.
-                    As someone committed to self-growth and ongoing learning, I am currently working on programming a new game having turned off the internal game physics. 
-                    All the physics is kinematically programmed and the game is state-based AI.
+                    write stuff here
 
                 </div>
                 <div style={{ padding: '20px', textAlign: 'left', color: 'gray' }}>
 
-                  <h5><u>My Role with our Capstone Project</u></h5>
+                  <h5><u>Recent Project updates</u></h5>
 
-                  I was the Programming Director and assisted the programmers in code or with design direction. 
-                  I was also the sole Networking programmer.
-                  Working as a team in several roles, I helped create multiple game play systems including the machines, 
-                  the item and item-grab programming, the game and audio manager, and the level manager, 
+                  write stuff here with dates and projectnames
 
 
                 </div>
