f4764a800df2580ef427c6769d3d521b49352a73
Brian Purdy <brianpurdy01@gmail.com>
2021-09-07 22:10:25 -0400

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,12 +8,12 @@ class Contact extends Component {
       <div style={{width: '100%', margin: 'auto'}} scroll>
         <Grid className="contact-grid">
           <Cell col={6}>
-            <h2>Brian Purdy</h2>
             <img
-              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              src={process.env.PUBLIC_URL + "/Images/MyIcons/knight_512x512.png"}
               alt="avatar"
-              style={{height: '250px'}}
-               />
+              style={{height: '250px', paddingBottom: '10px'}}
+              />
+              <h2>Brian Purdy</h2>
              <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
                </p>
