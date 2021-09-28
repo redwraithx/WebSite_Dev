5d0ffdd132749684f180ff952e98501808fee41e
Brian Purdy <brianpurdy01@gmail.com>
2021-09-28 04:47:27 -0400

    content update
    - hid some content on the about me page temporary
    - updated some misspellings

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -19,22 +19,23 @@ class About extends Component {
               
               <h5 className="aboutMeEntryHeader" ><u>About Me</u></h5>
               
-              I am a recent graduate of the Toronto Film School where I received a Diploma in Video Game Programming and Designer.<br />
-              Currently searching for work where being a part of a team means delivering great games and content to people who love to Game!<br />
-              I dream of being a part of a company that inspire the next generation of players and programmers alike while making great games.<br />
+              I am a recent graduate of the Toronto Film School where I received a Diploma in Video Game Programming and Designer. 
+              Currently searching for work where being a part of a team means delivering great games and content to people who love to Game! 
+              I dream of being a part of a company that inspire the next generation of players and programmers alike while making great games. 
               As someone committed to self-growth and ongoing learning, I am currently working on programming a new game having turned off the internal game physics. 
               All the physics is kinematically programmed and the game is using state-based AI.<br />
+               
 
             </div>
-            <div className="aboutMeEntryStyle2" style={{  }}>
+            <div className="aboutMeEntryStyle2" style={{  }} hidden >
 
               <h5 className="aboutMeEntryHeader" ><u>My Role with our Capstone Project: Wash Off</u></h5>
 
-              I was the Programming Director/Network programmer/General GamePlay programmer and assisted the programmers in code or with design direction.<br />
-              As the sole Networking programmer, I had a steep hill to climb but I did preveil with a lot of research and experimentation.<br />
-              I was also the sole Networking programmer.<br />
+              I was the Programming Director/Network programmer/General GamePlay programmer and assisted the programmers in code or with design direction. 
+              As the sole Networking programmer, I had a steep hill to climb but I did preveil with a lot of research and experimentation. 
+              I was also the sole Networking programmer. 
               Working as a team in several roles, I helped create multiple game play systems including the machines, the item and item-grab programming, the game and audio manager, and the level manager,
-
+               
 
             </div>
 
@@ -42,12 +43,12 @@ class About extends Component {
               
               <h5 className="aboutMeEntryHeader" ><u>Recent Work</u></h5>
               
-              I use to work as an I.T. Junior Systems Administrator at the N.S Halifax Convention Centre.<br />
-              This is where I really started to use my skills to make some of the everyday tasks I did more automated through little applications, like the network COM Room tester app in my c# projects.<br />
-              This saved me time, when I had to run around checking 23 different communication rooms to make sure they were online.<br />
-              I did this while we were still sourcing a network management solution and during the new buildings construction.<br />
-              When my contract was up I decided that I was going to follow my heart and transition into game development.<br />
+              I use to work as an I.T. Junior Systems Administrator at the N.S Halifax Convention Centre. 
+              This is where I really started to use my skills to make some of the everyday tasks I did more automated through little applications, like the network COM Room tester app in my c# projects.               This saved me time, when I had to run around checking 23 different communication rooms to make sure they were online.<br />
+              I did this while we were still sourcing a network management solution and during the new buildings construction. 
+              When my contract was up I decided that I was going to follow my heart and transition into game development. 
               This has been a great experience to not only learn from others who have worked in big and small studios, but also to lend my experiences and help those that are just starting out as well.<br />
+               
 
             </div>
 
